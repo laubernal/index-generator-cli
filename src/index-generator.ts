ab435e6af16eb9dfa25a2ab548ddcb955f32ecaa
laubernal <lbernalrodrguez@gmail.com>
2021-09-20 17:56:54 +0200

    structure the algorithm

--- src/index-generator.ts
+++ src/index-generator.ts
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
+import * as path from 'path';
 
 console.log('hi there');
+
+// Obtain current path
+
+// Implement DFS algorithm
+// Create a node class that through the constructor accepts an argument that gets assigned to the data property
+// and initialize an empty array for storing children. Methods add and remove --- add [export * from 'path']
+class TreeNode {
+  constructor(private data: any) {
+    const commandPath = path.dirname('');
+  }
+}
+
+// Create a tree class that the constructor initializes a root property to null. Method 'traverseDF' should
+// accept a function that gets called with each element in the tree
+class Tree {
+  constructor() {}
+}
+
+// Implement writeFile
