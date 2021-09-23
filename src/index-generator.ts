d99633d9f62cb4e56b8f3d0322e214af5b4e3624
laubernal <lbernalrodrguez@gmail.com>
2021-09-23 13:01:35 +0200

    writing the export line

--- src/index-generator.ts
+++ src/index-generator.ts
@@ -1,30 +1,59 @@
 #!/usr/bin/env node
 
-import * as path from 'path';
 import * as fs from 'fs';
 import { cwd } from 'process';
 
-console.log('hi there');
-
 // Obtain current path
 const currentPath = cwd();
-console.log('Current path', currentPath);
+// console.log('Current path', currentPath);
 
 const filenames = fs.readdirSync(currentPath);
 console.log('filenames:', filenames);
 
 const traverseDF = (fn: any) => {
-  while (filenames.length) {
+  while (filenames.length) { 
+    // Check if there is an index.ts file inside the directory
+    // const indexFile = filenames.find((filename: string) => {
+    //   return filename === 'index.ts';
+    // });
+
+    // Take the first one out
     const file = filenames.shift();
+    console.log('File:', file);
 
-    const filePath = `${currentPath}/${file}`;
+    const filePath = `${currentPath}\\${file}`;
+    // console.log('File Path: ', filePath);
 
+    // Obtain the stats of the file
     const stats = fs.lstatSync(filePath);
 
+    // Check if it is a directory
     if (stats.isDirectory()) {
+      // Insert all files of the directoy at the beginning of the array
       filenames.unshift(...fs.readdirSync(filePath));
       continue;
     }
+
+    console.log(filenames);
+
+    // It is a file -> write "export * from 'file'" inside the index.ts file
+    // Check if file is not undefined
+    if (file) {
+      const filenamesToExport = filenames.map(filename => {
+        return filename.split('.ts');
+      });
+      console.log('Filenames to export:', filenamesToExport);
+
+      const data = `\nexport * from './${filenamesToExport[0]}';`;
+      console.log('Data:', data);
+
+      fs.appendFileSync('index.ts', data, 'utf-8');
+    }
+    // There is no index.ts file, we have to create it
+    // else {
+    //   const data = `\nexport * from './${filenames}'`
+    //   fs.writeFileSync('index.ts', data, 'utf-8');
+    // }
 
     fn(file);
   }
@@ -61,3 +90,6 @@ traverseDF(console.log);
 // accept a function that gets called with each element in the tree
 
 // Implement writeFile
+
+
+//
