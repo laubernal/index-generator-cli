7ccf735d6fd6af68f006a04dce12de489d6bead6
laubernal <lbernalrodrguez@gmail.com>
2021-09-24 13:23:47 +0200

    pushing files in the array of the object pathMap

--- src/index-generator.ts
+++ src/index-generator.ts
@@ -2,61 +2,64 @@
 
 import * as fs from 'fs';
 import { cwd } from 'process';
+import path from 'path';
 
 // Obtain current path
-const currentPath = cwd();
+const commandPath = cwd();
 // console.log('Current path', currentPath);
 
-const filenames = fs.readdirSync(currentPath);
-console.log('filenames:', filenames);
+const filenames = fs.readdirSync(commandPath);
+console.log('Filenames:', filenames);
 
 const pathMap: { [key: string]: string[] } = {};
+const filesInsideDir: string[] = [];
 
 const traverseDF = (fn: any) => {
   while (filenames.length) {
-    // Check if there is an index.ts file inside the directory
-    // const indexFile = filenames.find((filename: string) => {
-    //   return filename === 'index.ts';
-    // });
-
     // Take the first one out
     const file = filenames.shift();
     console.log('File:', file);
 
     // Check if file is not undefined
     if (file) {
-      const filePath = `${currentPath}\\${file}`;
-      // console.log('File Path: ', filePath);
+      // Obtain the current path
+      const currentPath = fs.realpathSync(file);
+      console.log('Current Path: ', currentPath);
 
       // Obtain the stats of the file
-      const stats = fs.lstatSync(filePath);
+      const stats = fs.lstatSync(currentPath);
 
       // Check if it is a directory
       if (stats.isDirectory()) {
+        console.log('I am a directory');
+
         // Insert all files of the directoy at the beginning of the array
-        filenames.unshift(...fs.readdirSync(filePath));
+        filenames.unshift(...fs.readdirSync(currentPath));
         continue;
       }
 
-      // It is a file
-      // Save in the object the path of the file as a key and the filename as a value in an array
-      pathMap[currentPath] = [...pathMap[currentPath], file];
+      console.log('I am a file');
 
-      console.log(filenames);
+      // It is a file
+      // Take out the filename of the path
+      const dirPath = currentPath.replace(`\\${file}`, '');
+      // Save in the object the path of the file as a key and the filename as a value in an array
+      filesInsideDir.push(file);
+      pathMap[dirPath] = filesInsideDir;
+
+      console.log('Path Map:', pathMap);
+      console.log('Updated Filenames:', filenames);
 
       // It is a file -> write "export * from 'file'" inside the index.ts file
-      // Check if file is not undefined
-      if (file) {
-        const filenamesToExport = filenames.map(filename => {
-          return filename.split('.ts');
-        });
-        console.log('Filenames to export:', filenamesToExport);
+      const filenamesToExport = filenames.map(filename => {
+        return filename.split('.ts');
+      });
+      console.log('Filenames to export:', filenamesToExport);
 
-        const data = `\nexport * from './${filenamesToExport[0]}';`;
-        console.log('Data:', data);
+      const data = `\nexport * from './${filenamesToExport[0]}';`;
+      console.log('Data:', data);
 
-        fs.appendFileSync('index.ts', data, 'utf-8');
-      }
+      fs.appendFileSync('index.ts', data, 'utf-8');
       // There is no index.ts file, we have to create it
       // else {
       //   const data = `\nexport * from './${filenames}'`
