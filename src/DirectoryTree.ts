5a0ea0e26a147d57c483f8640a9db6c60fed8e26
laubernal <lbernalrodrguez@gmail.com>
2021-09-30 11:44:31 +0200

    refactor directory tree class

--- src/DirectoryTree.ts
+++ src/DirectoryTree.ts
@@ -1,3 +1,61 @@
+import * as fs from 'fs';
+import { cwd } from 'process';
+import path from 'path';
+
+import { DirectoryNode } from './DirectoryNode';
+import { DATA, DEFAULT_EXTENSION, INDEX_FILE } from './constants';
+
 export class DirectoryTree {
-    
+  public getCommandPath() {
+    return cwd();
+  }
+
+  public buildDirectoryTree(sourcePath: string) {
+    const source = new DirectoryNode(sourcePath);
+
+    const stack = [source];
+
+    while (stack.length) {
+      const currentElement = stack.shift();
+
+      if (currentElement) {
+        const descendants = fs.readdirSync(currentElement.path);
+
+        this.writeIndexFile(descendants, currentElement.path);
+
+        for (let descendant of descendants) {
+          const descendantPath = `${currentElement.path}\\${descendant}`;
+          const descendantNode = new DirectoryNode(descendantPath);
+          currentElement.children.push(descendantNode);
+
+          if (fs.statSync(descendantNode.path).isDirectory()) {
+            stack.push(descendantNode);
+          }
+        }
+      }
+    }
+
+    return source;
+  }
+
+  private writeIndexFile(descendants: string[], filePath: string): void {
+    const filesToExport = descendants
+      .filter(filename => {
+        return (
+          path.extname(filename) === this.fileExtension() &&
+          filename != INDEX_FILE.concat(this.fileExtension())
+        );
+      })
+      .map(filename => `${DATA}${filename.replace(`${this.fileExtension()}`, '')}';`)
+      .join('\n');
+
+    fs.writeFileSync(`${filePath}\\index.ts`, filesToExport);
+  }
+
+  private fileExtension(): string {
+    if (typeof process.argv.slice(2)[0] === 'undefined') {
+      return DEFAULT_EXTENSION;
+    }
+    return process.argv.slice(2)[0];
+  }
 }
--- src/index-generator.ts
+++ src/index-generator.ts
@@ -1,88 +1,25 @@
 #!/usr/bin/env node
 
-import * as fs from 'fs';
-import { cwd } from 'process';
-import path from 'path';
 import { Command } from 'commander';
 
-import { DirectoryNode } from './DirectoryNode';
-import { DATA, DEFAULT_EXTENSION, INDEX_FILE } from './constants';
-
-const commandPath = cwd();
-
-let fileExtension: string;
-
-if (typeof process.argv.slice(2)[0] === 'undefined') {
-  fileExtension = DEFAULT_EXTENSION;
-} else {
-  fileExtension = process.argv.slice(2)[0];
-}
-
-function buildDirectoryTree(sourcePath: string) {
-  // Get the first element of the Directory Tree
-  const source = new DirectoryNode(sourcePath);
-
-  // Create a stack to store each source element
-  const stack = [source];
-
-  while (stack.length) {
-    // Get the first element of the stack
-    const currentElement = stack.shift();
-
-    if (currentElement) {
-      // Get all elements from the current element
-      const descendants = fs.readdirSync(currentElement.path);
-
-      writeIndexFile(descendants, currentElement.path);
-
-      for (let descendant of descendants) {
-        // Get the path of each element
-        const descendantPath = `${currentElement.path}\\${descendant}`;
-        // Create a node for each element
-        const descendantNode = new DirectoryNode(descendantPath);
-        // Store the elements inside the node
-        currentElement.children.push(descendantNode);
-
-        if (fs.statSync(descendantNode.path).isDirectory()) {
-          // Store the directory at the stack
-          stack.push(descendantNode);
-        }
-      }
-    }
-  }
-
-  return source;
-}
-
-function writeIndexFile(descendants: string[], filePath: string): void {
-  const filesToExport = descendants
-    .filter(filename => {
-      return (
-        path.extname(filename) === fileExtension && filename != INDEX_FILE.concat(fileExtension)
-      );
-    })
-    .map(filename => `${DATA}${filename.replace(`${fileExtension}`, '')}';`)
-    .join('\n');
-
-  fs.writeFileSync(`${filePath}\\index.ts`, filesToExport);
-}
+import { DEFAULT_EXTENSION } from './constants';
+import { DirectoryTree } from './DirectoryTree';
 
 const program = new Command();
 
-program.option(
-  '-g, --generate [extension]',
-  `Index-generator is a command to create autommatically the index file to export
-  all files of your project. You can specify which files you want to take into account by indicating the file 
-  extension as a second argument of the command, example: index-generator .ts`,
-  DEFAULT_EXTENSION
-);
-
-program.parse(process.argv);
+program
+  .option(
+    '-g, --generate [extension]',
+    'creates the index file to export all .ts files of your project',
+    DEFAULT_EXTENSION
+  )
+  .parse(process.argv);
 
 const options = program.opts();
 
 if (options.generate) {
-  const directoryNode = buildDirectoryTree(commandPath);
+  const directoryNode = new DirectoryTree();
+  directoryNode.buildDirectoryTree(directoryNode.getCommandPath());
   // console.log(JSON.stringify(directoryNode, null, 2));
   console.log('Index files created successfully');
 }
